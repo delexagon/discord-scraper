7c48bacd9d7e87f59495ed953c6728f434f0d2a5
Sam DeMarrais <delexagon@gmail.com>
2024-04-09 03:03:24 -0400

    Delete extract_messages.sh

--- extract_messages.sh
+++ extract_messages.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-rm individual/*
-cat *.har | grep avatar > messages.txt
-split -l 1 messages.txt
-mv x* individual
-ls individual | xargs -I{} sh -c "cat individual/{} | cut -c 22- | rev | cut -c 2- | rev > individual/1{}.1"
-rm individual/x*
-python3 remove_smaller.py
-ls individual | xargs -I{} sh sed.sh individual/{}
-python3 organize.py > messages.txt
-rm individual/*
+
