ccc5250fc824636a3f4baaceac197c60ffdc3035
Sam DeMarrais <delexagon@gmail.com>
2024-04-09 03:04:26 -0400

    Delete remove_smaller.py

--- remove_smaller.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print('removing')
-files = [f for f in os.listdir('individual') if os.path.isfile("individual/"+f)]
-for f in files:
-    size = os.path.getsize("individual/"+f)
-    if size < 1000:
-        print("removing individual/"+f +": size = ", size)
-        os.remove("individual/"+f)
